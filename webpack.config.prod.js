d6161d88447c2a2211f3ffc3376852c0bd730c28
zhaozhuoboy <zhaozhuoboy@163.com>
2016-08-02 11:00:30 +0800

    modify package.js

---
+++ webpack.config.prod.js
@@ -0,0 +1,29 @@
+var path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src')
+    },
+    {
+      test: /\.css$/,
+      loaders: ['style','css','autoprefixer'],
+    },
+    {
+      test: /\.(jpe?g|gif|png|svg)$/,
+      loaders: ['file-loader']
+    },
+    {
+      test: /\.(woff|woff2|ttf|svg|eot)(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000"
+    }
+  ]
+  }
+};
