d829c8a38c5d1e7da0eedd28d52e2e1a8d730c30
zhaozhuoboy <zhaozhuoboy@163.com>
2016-08-06 15:32:18 +0800

    i

--- src/components/notelist/CardList.js
+++ src/components/notelist/CardList.js
@@ -1,15 +1,10 @@
 import React, { PropTypes } from 'react';
 import NoteCard from './NoteCard.js';
 import map from 'lodash/fp/map';
+import axios from 'axios';
 
-let listData =[
-  {index:"1",title:"html学习笔记",date:"2016.07.12"},
-  {index:"2",title:"box-shadow 学习笔记",date:"2016.07.13"},
-  {index:"3",title:"css4学习笔记",date:"2016.07.15"},
-  {index:"4",title:"javascript学习笔记",date:"2016.07.16"},
-  {index:"5",title:"webpack环境搭建",date:"2016.07.22"}
 
-];
+//https://raw.githubusercontent.com/zhaozhuoboy/big-demo/master/posts/blogs.json
 
 
 
@@ -22,33 +17,60 @@ let listData =[
 //      />);
 //   }
 class CardList extends React.Component {
-  render () {
-    let cardlist =[];
-    if(this.props.pipei == ""){
-      console.log('weikong');
-      map((a) => {
-          cardlist.push(
-            <NoteCard index={a.index}
-                      title={a.title}
-                      date={a.date}
-                      key={Math.random()}
-              />
-          );
-      },listData );
-    }else{
-      let query=new RegExp(this.props.pipei,"i");
-      for (var i = 0; i < listData.length; i++) {
-
-        if (query.test(listData[i].title)||query.test(listData[i].index)) {
-          cardlist.push(<NoteCard title={listData[i].title} date={listData[i].date} index={listData[i].index} key={Math.random()}/>)
-        }
-      }
+  constructor(){
+    super();
+    this.state={
+      posts:''
     }
+  }
+  componentDidMount(){
+    let address = `https://raw.githubusercontent.com/zhaozhuoboy/big-demo/master/posts/blogs.json`
+    axios.get(address).then((res) => {
+      console.log(res);
+      this.setState({
+        posts: res.data
+      });
+    });
+  }
+  render () {
+    var blogCards = [];
+    map((b) =>  {
+                  blogCards.push(
+                    <NoteCard title={b.title} date={b.created_at } index={b.id} key={Math.random()}/>
+                  );
+                },
+        this.state.posts
+    );
+
+
+    // console.log(this.state.posts.length);
+    // let cardlist =[];
+    // if(this.props.pipei == ""){
+    //   console.log('weikong');
+      // map((a) => {
+      //               cardlist.push(
+      //                 <NoteCard index={a.index}
+      //                           title={a.title}
+      //                           date={a.date}
+      //                           key={Math.random()}
+      //                   />
+      //               );
+      //           },
+      //           this.state.posts );
+    // }else{
+    //   let query=new RegExp(this.props.pipei,"i");
+    //   for (var i = 0; i < this.state.posts.length; i++) {
+    //
+    //     if (query.test(this.state.posts[i].title)||query.test(this.state.posts[i].index)) {
+    //       cardlist.push(<NoteCard title={this.state.posts[i].title} date={this.state.posts[i].date} index={this.state.posts[i].index} key={Math.random()}/>)
+    //     }
+    //   }
+    // }
 
     return(
       <div>
 
-        {cardlist}
+        {blogCards}
       </div>
 
     )
