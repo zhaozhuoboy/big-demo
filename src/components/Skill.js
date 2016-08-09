bf589ea664535e908de47aca915ddf785f324d43
zhaozhuoboy <zhaozhuoboy@163.com>
2016-08-09 10:23:52 +0800

    skill

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -5,43 +5,49 @@ class Skill extends React.Component {
   componentDidMount(){
     let mySkill = Echarts.init(document.getElementById('mySkill'));
     mySkill.setOption({
-    title : {
-        text: '技能掌握',
-        x:'center',
-        textStyle:{
-          fontSize:30
+    color: ['#2f4554', '#61a0a8', '#d48265', '#91c7ae','#749f83',  '#ca8622', '#bda29a','#6e7074', '#546570', '#c4ccd3'],
+    title:{
+      text:"我的技能展示",
+      textStyle:{
+        fontSize:20
+      },
+      left:'center',
+
+    },
+    left:"44%",
+    tooltip : {
+        trigger: 'axis',
+        axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+            type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
         }
     },
-    tooltip : {
-        trigger: 'item',
-        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
     },
-    legend: {
-        orient: 'vertical',
-        left: 'left',
-        data: ['HTML5','CSS3','JavaScript','PhotoShop','JQuery','React']
-    },
+    xAxis : [
+        {
+            type : 'category',
+            data : ['HTML5', 'CSS3', 'PhotoShop', 'JavaScript', 'React', 'JQuery'],
+            axisTick: {
+                alignWithLabel: true
+            }
+        }
+    ],
+    yAxis : [
+        {
+            type : 'value',
+            name:'百分比%'
+        }
+    ],
     series : [
         {
-            name: '访问来源',
-            type: 'pie',
-            radius : '55%',
-            center: ['50%', '60%'],
-            data:[
-                {value:370, name:'HTML5'},
-                {value:310, name:'CSS3'},
-                {value:234, name:'JavaScript'},
-                {value:135, name:'PhotoShop'},
-                {value:135, name:'JQuery'},
-                {value:160, name:'React'}
-            ],
-            itemStyle: {
-                emphasis: {
-                    shadowBlur: 10,
-                    shadowOffsetX: 0,
-                    shadowColor: 'rgba(0, 0, 0, 0.5)'
-                }
-            }
+            name:'技能熟练度',
+            type:'bar',
+            barWidth: '46%',
+            data:[94, 90,86, 89, 56, 70]
         }
     ]
 });
@@ -50,14 +56,14 @@ class Skill extends React.Component {
     let style={
       root:{
         width:"80%",
-        margin:"30px auto",
+        margin:"60px auto",
         padding:"70px 0",
-        boxShadow:"3px 2px 5px rgba(0,0,0,0.5)",
+        boxShadow:"0px 2px 5px rgba(0,0,0,0.5)",
         backgroundColor:"#fff"
       },
       skill:{
         width:"80%",
-        height:"400px",
+        minHeight:"500px",
         margin:"0 auto"
       }
     }
