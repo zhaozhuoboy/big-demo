de821c4c73fc3fc4dbac61b20dbb295adb901002
zhaozhuoboy <zhaozhuoboy@163.com>
2016-08-09 10:25:42 +0800

    i

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { PropTypes } from 'react';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import NavBar from './components/NavBar';
-// import Header from './components/Header';
 import Footer from './components/Footer';
 
 
@@ -9,17 +8,6 @@ class App extends React.Component {
   getChildContext() {
     return {muiTheme: getMuiTheme()};
   }
-  // constructor(){
-  //   super();
-  //   this.state={
-  //     search:""
-  //   }
-  // }
-  // cardSearch(inputTxt){
-  //   this.setState({
-  //     search:inputTxt
-  //   })
-  // }
   render () {
     return(
       <div>
